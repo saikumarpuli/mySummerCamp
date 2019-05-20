2e2e013b97a32ad52fb06c93a7c28acb077f779d
saikumarpuli <saikumarpuli9@gmail.com>
2019-05-20 16:16:43 +0530

    Serverside searching minor bugs fixed is completed

--- client/mySummerCamp/src/app/studentdetails/studentdetails.component.ts
+++ client/mySummerCamp/src/app/studentdetails/studentdetails.component.ts
@@ -20,23 +20,6 @@ export class StudentdetailsComponent implements OnInit {
   ngOnInit() {
     this.getdata()
   }
-
-  getPagination(pageNumber) {
-    this.pageno = pageNumber;
-    this.page = {
-      pageNo: this.pageno,
-      itemsPerPage: this.itemsperpage,
-      course:this.search.course,
-      firstName:this.search.firstName,
-      lastName:this.search.lastName,
-      email:this.search.email
-     };
-    this.service.getStudentDetails(this.page).subscribe((response) => {
-      this.students = response.rows;
-      this.total = response.count;
-    })
-  }
-
   getdata() {
     this.page = {
       pageNo: this.pageno,
@@ -46,6 +29,9 @@ export class StudentdetailsComponent implements OnInit {
       lastName:this.search.lastName,
       email:this.search.email
     };
+
+    console.log(this.page,"page");
+    console.log(this.search,"search")
     this.service.getStudentDetails(this.page).subscribe((response) => {
       this.students = response.rows;
       this.total = response.count;
@@ -53,4 +39,23 @@ export class StudentdetailsComponent implements OnInit {
 
   }
 
-}
+    getPagination(pageNumber) {
+      this.pageno = pageNumber;
+      this.page = {
+        pageNo: this.pageno,
+        itemsPerPage: this.itemsperpage,
+        course:this.search.course,
+        firstName:this.search.firstName,
+        lastName:this.search.lastName,
+        email:this.search.email
+      };
+
+      console.log(this.page,"page1");
+      console.log(this.search,"search1")
+      this.service.getStudentDetails(this.page).subscribe((response) => {
+        this.students = response.rows;
+        this.total = response.count;
+      })
+    }
+  }
+
