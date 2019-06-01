66e1ff3e254f6402985aae8109359a3063b11bae
saikumarpuli <saikumarpuli9@gmail.com>
2019-06-01 17:41:29 +0530

    Serverside searching minor bugs fixed is completed

--- client/mySummerCamp/server.js
+++ client/mySummerCamp/server.js
@@ -1,7 +1,7 @@
 const express =require('express'),
   path=require('path');
 const  app =express();
-app.use(express.static('./dist/my-summer-camp'));
+app.use(express.static('./dist/mysummercamp'));
 app.get('/*', (req,res)=>{
-  res.sendFile(path.json(__dirname,'/dist/my-summer-camp/index.html'));})
+  res.sendFile(path.json(__dirname,'/dist/mysummercamp/index.html'));})
 app.listen(process.env.PORT||8080,()=>{console.log('server started')})
