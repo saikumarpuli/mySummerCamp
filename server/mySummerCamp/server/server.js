ffea791f1a224718ba95ab9dd52eb47585768f2e
saikumarpuli <saikumarpuli9@gmail.com>
2019-06-04 15:36:54 +0530

    Serverside searching minor bugs fixed is completed

--- server/mySummerCamp/server/server.js
+++ server/mySummerCamp/server/server.js
@@ -4,13 +4,13 @@ import os from "os";
 import express from "express";
 import http from "http";
 import RoutesConfig from "./config/routes.conf";
-import DBConfig from "./config/db.conf";
+// import DBConfig from "./config/db.conf";
 import Routes from "./routes/index";
 
 const app = express();
 
 RoutesConfig.init(app);
-DBConfig.init();
+// DBConfig.init();
 Routes.init(app, express.Router());
 
 http.createServer(app)
