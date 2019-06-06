5b94807cacb110321f878e2e9080650af74010aa
saikumarpuli <saikumarpuli9@gmail.com>
2019-06-06 16:06:24 +0530

    sucessfully push project to heroku

--- client/mySummerCamp/server.js
+++ client/mySummerCamp/server.js
@@ -3,5 +3,5 @@ const express =require('express'),
 const  app =express();
 app.use(express.static('./dist/mySummerCamp'));
 app.get('/*', (req,res)=>{
-  res.sendFile(path.json(__dirname,'/dist/mySummerCamp/index.html'));})
+  res.sendFile(path.join(__dirname,'/dist/mySummerCamp/index.html'));})
 app.listen(process.env.PORT||8080,()=>{console.log('server started')})
